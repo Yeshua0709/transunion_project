ca06bda6157573c7968e7964b38204c1e2cfe69d
Yeshua Miguel Abrenica <abrenica.y@gmail.com>
2024-06-06 15:26:52 +0800

    routing

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ main{
     </a> </div>
 
 
-            <div class="li"><a class="nav-active" href="">
+            <div class="li"><a class="nav-active" href="#">
                 
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-postcard" viewBox="0 0 16 16">
                  <path fill-rule="evenodd" d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm7.5.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0zM2 5.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5M10.5 5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM13 8h-2V6h2z"/>
@@ -133,7 +133,7 @@ main{
             Dashboard 
 </a> </div>
 
-            <div class="li"><a href="">
+            <div class="li"><a href="view/units.php">
 
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
                <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5"/>
@@ -145,7 +145,7 @@ main{
         </a></div>
 
 
-            <div class="li"><a href="">
+            <div class="li"><a href="view/employees.php">
                 
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
                  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
--- view/login.php
+++ view/login.php
@@ -25,18 +25,131 @@ $errorMessage = "";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link rel="stylesheet" href="../styles/font.css">
 </head>
+
+<style>
+
+*{
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
+}
+.login-container{
+width: 100%;
+height:100vh;
+background:#00A6CA;
+display:flex;
+align-items: center;
+justify-content: center;
+}
+
+.login-card{
+    margin:0 auto; 
+background:white;
+padding:2em;
+border-radius: 10px;
+box-shadow: 0 3px 5px rgb(0,0,0,0.3);
+}
+
+
+.login-card form{
+    width:100%;
+}
+.login-header{
+display:flex;
+margin: 0 auto;
+}
+
+
+.logo-holder img{
+width:70px;
+height:auto;
+margin-right:1em;
+}
+
+
+.form-row input{
+    width:100%;
+    padding:1em;
+    margin-bottom:2em;
+    border:none;
+   
+}
+
+.input-shadow{
+    box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+}
+
+.login-btn{
+    background:none;
+    border:none;
+    background:#FCD800;
+    color:white;
+    font-weight: 600;
+    border-radius:5px;
+    box-shadow: 0 3px 5px rgb(0,0,0,0.1);
+    cursor:pointer;
+}
+
+.header-container h1{
+    color:#00A6CA;
+}
+
+.header-container p{
+    color: rgb(0,0,0,0.3);
+
+}
+</style>
+
+
 <body>
 
+<div class="login-container">
 
-<h1> Login Page </h1>
 
-<form method ="POST" action ="../controller/login_process.php">
+<div class="login-card">
+    <form method ="POST" action ="../controller/login_process.php">
 
-<input type="text" name="username" placeholder="Enter Username">
-<input type="password" name="password" placeholder="Enter Password">
 
-<input type="submit" value="login" name="login">
+    <div class="login-header">
+
+    <div class="logo-holder">
+        <img class="logo" src="../assets/logo.png">
+    </div>
+
+
+    <div class="header-container">
+
+     <h1>Sign-in</h1>
+     <p> Please enter your credentials to continue.</p>
+    </div>
+    </div>
+
+<br>
+<br>
+<br>
+    <div class="form-row">
+     <input class="input-shadow" type="text" name="username" placeholder="Enter Username">
+    </div>
+
+    <div class="form-row">
+    <input class="input-shadow" type="password" name="password" placeholder="Enter Password">
+        </div>
+
+
+        <div class="form-row">
+        <input class="login-btn" type="submit" value="SIGN-IN" name="login">
+        </div>
+
+
+</div>
+
+
+</div>
+
+
 
 
 
